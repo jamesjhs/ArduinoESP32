ff8decce6db8e76fae6c018354a44b836dac7515
jamesjhs <jrowson@gmail.com>
2024-12-17 00:45:09 +0000

    added motion sensor

--- esp32AdvancedWebServer/esp32AdvancedWebServer.ino
+++ esp32AdvancedWebServer/esp32AdvancedWebServer.ino
@@ -1,31 +1,48 @@
-// DOIT ESP32 DEVKIT V1 
+// DOIT ESP32 DEVKIT V1
 
 #include <WiFi.h>
 #include <NetworkClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
+//WiFi Setup variables
 const char *ssid = "Kramig2.4";
 const char *password = "7e988e0be3";
+
+// Pins required
+const int led = 2;         //inbuilt
+const int ledReed = 32;    //D32
+const int ledMotion = 33;  //D33
+const int pinSwitch = 34;  //D34
+const int pinMotion = 35;  //D35
+
+// WHEEL setup variables
 int switchPressed = 0;
 int switchCount = 0;
-float wheelDia = 13.5; // diesel's wheel diameter (cm)
-float wheelCirfumf = 3.1415 * wheelDia / 100; // wheel circumference (m)
-float distTravelled; 
-int writeDelay = 2; // minutes
+float wheelDia = 13.5;                         // diesel's wheel diameter (cm)
+float wheelCirfumf = 3.1415 * wheelDia / 100;  // wheel circumference (m)
+float distTravelled;
+unsigned long wheelLast;
+
+// MOTION setup variables
+int motionActive = 0;
+int motionCount = 0;
+unsigned long motionLast;
+
+// Output/processing variables
+int writeDelay = 2;  // minutes
 //int writeDelayMsec = writeDelay * 60 * 1000; // milliseconds
-int writeDelayMsec = 2000; // milliseconds
+int writeDelayMsec = 2000;  // milliseconds
 int writeTime;
 int arraySpins[200][2] = {};
 
 WebServer server(80);
 
-const int led = 2;
-const int pinSwitch = 34;
-
 void resetData() {
   switchCount = 0;
   distTravelled = 0;
+  motionCount = 0;
+  arraySpins[200][2] = { 0 };
 }
 
 void dataPhp() {  // when data.php is called,
@@ -34,9 +51,11 @@ void dataPhp() {  // when data.php is called,
   int sec = millis() / 1000;
   int hr = sec / 3600;
   int min = (sec / 60) % 60;
-  float distance = switchCount * 13.5/100 * 3.1415;
-
   sec = sec % 60;
+
+  float distance = switchCount * 13.5 / 100 * 3.1415;
+  int motionSince = (millis() - motionLast) / 1000;
+  int wheelSince = (millis() - wheelLast) / 1000;
 
   snprintf(
     temp, 400,
@@ -44,29 +63,30 @@ void dataPhp() {  // when data.php is called,
     "<html>\
   <head>\
     <meta http-equiv='refresh' content='1'/>\
-    <title>Diesel's Wheel</title>\
+    <title>Diesel the Hamster</title>\
     <style>\
       body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
     </style>\
   </head>\
   <body>\
-    <h1>Diesel's Wheel</h1>\
+    <h1>Diesel the Hamster</h1>\
     <p>Uptime: %02d:%02d:%02d</p>\
-    <p>Reed switch state: %01d | Distance travelled (m): %1.2f</p>\
+    <p>Motion triggered: %01d (%01d sec ago) | Distance travelled (m): %1.2f (%01d sec ago)</p>\
   </body>\
 </html>",
 
-    hr, min, sec, switchPressed, distance);
+    hr, min, sec, motionCount, motionSince, distance, wheelSince);
   server.send(200, "text/html", temp);
 }
 
 void handleRoot() {
 
-  char temp[400];
   int sec = millis() / 1000;
   int hr = sec / 3600;
   int min = (sec / 60) % 60;
   sec = sec % 60;
+
+  char temp[400];
 
   snprintf(
     temp, 400,
@@ -121,7 +141,10 @@ void handleNotFound() {
 
 void setup(void) {
   pinMode(led, OUTPUT);
+  pinMode(ledReed, OUTPUT);
+  pinMode(ledMotion, OUTPUT);
   pinMode(pinSwitch, INPUT);
+  pinMode(pinMotion, INPUT);
 
   digitalWrite(led, 0);
   Serial.begin(115200);
@@ -166,30 +189,53 @@ void loop(void) {
   server.handleClient();
   delay(2);  //allow the cpu to switch to other tasks
 
+  int sec = millis() / 1000;
+  int hr = sec / 3600;
+  int min = (sec / 60) % 60;
+  sec = sec % 60;
+
   if (digitalRead(pinSwitch)) {
     if (switchPressed == 0) {
-      digitalWrite(led, 1);
+      digitalWrite(ledReed, 1);
       delay(10);
-      digitalWrite(led, 0);
+      digitalWrite(ledReed, 0);
       switchPressed = 1;
+      wheelLast = millis();
+      Serial.print("Wheel spin at ");
+      Serial.println(millis()/1000);
       switchCount++;
     }
   } else {
     if (switchPressed == 1) {
       switchPressed = 0;
-    }  
-  } //end if pinSwitch
+    }
+  }  //end if pinSwitch
+
+
+  if (digitalRead(pinMotion)) {
+    if (motionActive == 0) {
+      digitalWrite(ledMotion, 1);
+      motionActive = 1;
+      motionLast = millis();
+      Serial.print("Motion detected at ");
+      Serial.println(millis()/1000);
+      motionCount++;
+    }
+  } else {
+    if (motionActive == 1) {
+      motionActive = 0;
+      digitalWrite(ledMotion, 0);
+    }
+  }  //end if pinSwitch
 
   if (writeTime < millis()) {
     writeTime = millis() + writeDelayMsec;
-
-
   }
 
 
-} //end LOOP()
+}  //end LOOP()
 
-void drawGraph() { // test.svg
+void drawGraph() {  // test.svg
   String out = "";
   char temp[100];
   out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
@@ -208,14 +254,14 @@ void drawGraph() { // test.svg
   server.send(200, "image/svg+xml", out);
 }
 
-void drawGraph2() { // // graph.svg
+void drawGraph2() {  // // graph.svg
   String out = "";
   char temp[100];
   out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
   out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
   out += "<g stroke=\"black\">\n";
 
-/*  SYNTAX of the line elements, one by one. Previous line's x2/y2 equals new line's x1/y1: 
+  /*  SYNTAX of the line elements, one by one. Previous line's x2/y2 equals new line's x1/y1: 
 
 <line x1="10" y1="124" x2="20" y2="132" stroke-width="1" />
 <line x1="20" y1="132" x2="30" y2="79" stroke-width="1" />
